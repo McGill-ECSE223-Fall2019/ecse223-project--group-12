60f700788924211b59328c48732d66e81cdd274d
franciscomeau <francis.comeau@mail.mcgill.ca>
2019-10-09 19:19:03 -0400

    Readded my 2 features in controller

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/QuoridorController.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/QuoridorController.java
@@ -2,6 +2,7 @@ package ca.mcgill.ecse223.quoridor.controller;
 
 import ca.mcgill.ecse223.quoridor.QuoridorApplication;
 import ca.mcgill.ecse223.quoridor.model.Game;
+import ca.mcgill.ecse223.quoridor.model.GamePosition;
 import ca.mcgill.ecse223.quoridor.model.Player;
 import ca.mcgill.ecse223.quoridor.model.Quoridor;
 import ca.mcgill.ecse223.quoridor.model.User;
@@ -27,6 +28,22 @@ public class QuoridorController {
 	
 	public static void setWhitePlayer() {
 		
+	}
+	
+	/**
+	 * @author Francis Comeau
+	 * Gherkin feature: LoadPosition.feature
+	 */
+	public static void loadPosition(String fullPath) throws java.lang.UnsupportedOperationException {
+		throw new java.lang.UnsupportedOperationException(); 
+	}
+	
+	/**
+	 * @author Francis Comeau
+	 * Gherkin feature: SavePosition.feature
+	 */
+	public static void savePosition(GamePosition gamePosition) throws java.lang.UnsupportedOperationException {
+		throw new java.lang.UnsupportedOperationException(); 
 	}
 
 
