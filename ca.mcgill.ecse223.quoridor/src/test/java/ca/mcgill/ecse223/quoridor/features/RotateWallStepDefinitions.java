d72298881416fc8a65c67d7f08c1b344c5d1dcd9
zechen.ren@mail.mcgill.ca <zechen.ren@mail.mcgill.ca>
2019-10-11 15:24:15 -0400

    RotateWallUpdate

--- ca.mcgill.ecse223.quoridor/src/test/java/ca/mcgill/ecse223/quoridor/features/RotateWallStepDefinitions.java
+++ ca.mcgill.ecse223.quoridor/src/test/java/ca/mcgill/ecse223/quoridor/features/RotateWallStepDefinitions.java
@@ -1,24 +1,42 @@
 package ca.mcgill.ecse223.quoridor.features;
 
+import ca.mcgill.ecse223.quoridor.QuoridorApplication;
+import ca.mcgill.ecse223.quoridor.controller.QuoridorController;
+import ca.mcgill.ecse223.quoridor.model.Direction;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import static org.junit.Assert.assertEquals;
 
-/*
- * Note that the classes may not conatain all relative steps for a certain feature, 
- * since there are duplicate steps between scenarios.
- */
+
 
 public class RotateWallStepDefinitions {
 	
 	@When("I try to flip the wall")
 	public void i_try_to_flip_the_wall() {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
+		try {
+			QuoridorController.rotateWall();
+		} catch (java.lang.UnsupportedOperationException e) {
+		    throw new cucumber.api.PendingException();
+		}
 	}
 
+	
 	@Then("The wall shall be rotated over the board to {string}")
-	public void the_wall_shall_be_rotated_over_the_board_to(String newDir) {
+	public void the_wall_shall_be_rotated_over_the_board_to(String string) {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
+		try {
+			switch (string) {
+			case "horizontal" :
+				assertEquals(Direction.Horizontal, QuoridorApplication.getQuoridor().
+					getCurrentGame().getWallMoveCandidate().getWallDirection());
+			case "vertical":
+				assertEquals(Direction.Vertical, QuoridorApplication.getQuoridor().
+						getCurrentGame().getWallMoveCandidate().getWallDirection());
+			}	
+			} catch (java.lang.UnsupportedOperationException e) {
+				throw new cucumber.api.PendingException();
+			}
+			
 	}
 }
