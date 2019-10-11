030e38c20170c6d1f0d3d485c0987d4271025f06
zechen.ren@mail.mcgill.ca <zechen.ren@mail.mcgill.ca>
2019-10-11 17:03:52 -0400

    Update MoveWallStepDefinitions.java

--- ca.mcgill.ecse223.quoridor/src/test/java/ca/mcgill/ecse223/quoridor/features/MoveWallStepDefinitions.java
+++ ca.mcgill.ecse223.quoridor/src/test/java/ca/mcgill/ecse223/quoridor/features/MoveWallStepDefinitions.java
@@ -1,8 +1,14 @@
 package ca.mcgill.ecse223.quoridor.features;
 
+import ca.mcgill.ecse223.quoridor.QuoridorApplication;
+import ca.mcgill.ecse223.quoridor.model.Direction;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotEquals;
+
 
 /*
  * Note that the classes may not conatain all relative steps for a certain feature, 
@@ -10,16 +16,43 @@ import io.cucumber.java.en.When;
  */
 
 public class MoveWallStepDefinitions {
-
 	@Given("A wall move candidate exists with {string} at position \\({int}, {int})")
 	public void a_wall_move_candidate_exists_with_at_position(String dir, Integer row, Integer col) {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
+		boolean existWall = false;
+		switch(dir){
+		case "horizontal":
+			if(Direction.Horizontal == QuoridorApplication.getQuoridor().getCurrentGame().getWallMoveCandidate().getWallDirection()
+				&& QuoridorApplication.getQuoridor().getCurrentGame().getWallMoveCandidate().getTargetTile().getRow() == row
+				&& QuoridorApplication.getQuoridor().getCurrentGame().getWallMoveCandidate().getTargetTile().getColumn() == col) {
+			existWall = true;
+			break;
+			}
+		case "vertical":
+			if(Direction.Vertical == QuoridorApplication.getQuoridor().getCurrentGame().getWallMoveCandidate().getWallDirection()
+			&& QuoridorApplication.getQuoridor().getCurrentGame().getWallMoveCandidate().getTargetTile().getRow() == row
+			&& QuoridorApplication.getQuoridor().getCurrentGame().getWallMoveCandidate().getTargetTile().getColumn() == col) {
+		existWall = true;
+		break;	
+		}
+		}
+		assertTrue(existWall);
+		throw new cucumber.api.PendingException();
 	}
 
 	@Given("The wall candidate is not at the {string} edge of the board")
 	public void the_wall_candidate_is_not_at_the_edge_of_the_board(String side) {
 	    // Write code here that turns the phrase above into concrete actions
+		switch (side) {
+		case "right":
+			assertNotEquals(8, QuoridorApplication.getQuoridor().getCurrentGame().getWallMoveCandidate().getTargetTile().getColumn());
+		case "left":
+			assertNotEquals(1, QuoridorApplication.getQuoridor().getCurrentGame().getWallMoveCandidate().getTargetTile().getColumn());
+		case "up":
+			assertNotEquals(1, QuoridorApplication.getQuoridor().getCurrentGame().getWallMoveCandidate().getTargetTile().getRow());
+		case "down":
+			assertNotEquals(8, QuoridorApplication.getQuoridor().getCurrentGame().getWallMoveCandidate().getTargetTile().getRow());
+		}
 	    throw new cucumber.api.PendingException();
 	}
 
@@ -38,12 +71,41 @@ public class MoveWallStepDefinitions {
 	@Then("A wall move candidate shall exist with {string} at position \\({int}, {int})")
 	public void a_wall_move_candidate_shall_exist_with_at_position(String dir, Integer row, Integer col) {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
-	}
+		boolean existWall = false;
+		switch(dir){
+		case "horizontal":
+			if(Direction.Horizontal == QuoridorApplication.getQuoridor().getCurrentGame().getWallMoveCandidate().getWallDirection()
+				&& QuoridorApplication.getQuoridor().getCurrentGame().getWallMoveCandidate().getTargetTile().getRow() == row
+				&& QuoridorApplication.getQuoridor().getCurrentGame().getWallMoveCandidate().getTargetTile().getColumn() == col) {
+			existWall = true;
+			break;
+			}
+		case "vertical":
+			if(Direction.Vertical == QuoridorApplication.getQuoridor().getCurrentGame().getWallMoveCandidate().getWallDirection()
+			&& QuoridorApplication.getQuoridor().getCurrentGame().getWallMoveCandidate().getTargetTile().getRow() == row
+			&& QuoridorApplication.getQuoridor().getCurrentGame().getWallMoveCandidate().getTargetTile().getColumn() == col) {
+		existWall = true;
+		break;	
+		}
+		}
+		assertTrue(existWall);
+		throw new cucumber.api.PendingException();
+}
+
 
 	@Given("The wall candidate is at the {string} edge of the board")
 	public void the_wall_candidate_is_at_the_edge_of_the_board(String dir) {
 	    // Write code here that turns the phrase above into concrete actions
+		switch (dir) {
+		case "right":
+			assertEquals(8, QuoridorApplication.getQuoridor().getCurrentGame().getWallMoveCandidate().getTargetTile().getColumn());
+		case "left":
+			assertEquals(1, QuoridorApplication.getQuoridor().getCurrentGame().getWallMoveCandidate().getTargetTile().getColumn());
+		case "up":
+			assertEquals(1, QuoridorApplication.getQuoridor().getCurrentGame().getWallMoveCandidate().getTargetTile().getRow());
+		case "down":
+			assertEquals(8, QuoridorApplication.getQuoridor().getCurrentGame().getWallMoveCandidate().getTargetTile().getRow());
+		}
 	    throw new cucumber.api.PendingException();
 	}
 
