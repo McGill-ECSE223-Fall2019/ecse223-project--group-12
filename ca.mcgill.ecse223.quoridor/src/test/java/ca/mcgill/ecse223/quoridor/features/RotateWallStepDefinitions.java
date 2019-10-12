65c7bea935c6913e6165a46ff51d5b29c488db28
zechen.ren@mail.mcgill.ca <zechen.ren@mail.mcgill.ca>
2019-10-11 22:40:40 -0400

    Update RotateWallStepDefinitions.java

--- ca.mcgill.ecse223.quoridor/src/test/java/ca/mcgill/ecse223/quoridor/features/RotateWallStepDefinitions.java
+++ ca.mcgill.ecse223.quoridor/src/test/java/ca/mcgill/ecse223/quoridor/features/RotateWallStepDefinitions.java
@@ -3,9 +3,17 @@ package ca.mcgill.ecse223.quoridor.features;
 import ca.mcgill.ecse223.quoridor.QuoridorApplication;
 import ca.mcgill.ecse223.quoridor.controller.QuoridorController;
 import ca.mcgill.ecse223.quoridor.model.Direction;
+import ca.mcgill.ecse223.quoridor.model.Game;
+import ca.mcgill.ecse223.quoridor.model.Player;
+import ca.mcgill.ecse223.quoridor.model.PlayerPosition;
+import ca.mcgill.ecse223.quoridor.model.Wall;
+import ca.mcgill.ecse223.quoridor.util.TestUtil;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
 
 
 /**
@@ -28,18 +36,26 @@ public class RotateWallStepDefinitions {
 	@Then("The wall shall be rotated over the board to {string}")
 	public void the_wall_shall_be_rotated_over_the_board_to(String string) {
 	    // Write code here that turns the phrase above into concrete actions
-		try {
-			switch (string) {
-			case "horizontal" :
-				assertEquals(Direction.Horizontal, QuoridorApplication.getQuoridor().
-					getCurrentGame().getWallMoveCandidate().getWallDirection());
-			case "vertical":
-				assertEquals(Direction.Vertical, QuoridorApplication.getQuoridor().
-						getCurrentGame().getWallMoveCandidate().getWallDirection());
-			}	
-			} catch (java.lang.UnsupportedOperationException e) {
-				throw new cucumber.api.PendingException();
+		boolean rotateWall = false;
+		Game game = QuoridorApplication.getQuoridor().getCurrentGame();
+		PlayerPosition playerPosition = TestUtil.getPlayerPositionByColor
+				(game.getCurrentPosition().getPlayerToMove().getUser().getName());
+		Player player = TestUtil.getPlayerByColor
+				(game.getCurrentPosition().getPlayerToMove().getUser().getName());
+		List<Wall> allWall = player.getWalls();
+		int row = playerPosition.getTile().getRow();
+		int col = playerPosition.getTile().getColumn();
+		for (int i = 0; i < allWall.size(); i++) {
+			if (allWall.get(i).getMove().getTargetTile().getColumn() == col
+				&& allWall.get(i).getMove().getTargetTile().getRow() == row
+				&& allWall.get(i).getMove().getWallDirection() == TestUtil.getDirection(string)) {
+				rotateWall = true;
+				break;
 			}
+		}
+		
+		assertTrue(rotateWall);
+		}
 			
 	}
 }
