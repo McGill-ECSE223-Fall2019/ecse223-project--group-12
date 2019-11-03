911e7399b2af66026a8b2671315d6fb769ac15b7
unknown <remi.carriere@mail.mcgill.ca>
2019-11-03 17:50:42 -0500

    Final revier before submission closes #13 closes #12 closes #11 closes #10 closes #9 closes #8

--- ca.mcgill.ecse223.quoridor/src/test/java/ca/mcgill/ecse223/quoridor/features/InitializeBoardStepDefinitions.java
+++ ca.mcgill.ecse223.quoridor/src/test/java/ca/mcgill/ecse223/quoridor/features/InitializeBoardStepDefinitions.java
@@ -1,7 +1,6 @@
 package ca.mcgill.ecse223.quoridor.features;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.sql.Time;
